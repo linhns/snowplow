69d6731dccf2f808cb872bf46c09ce68883c3500
Nguyen Son Linh <linhns59@gmail.com>
2025-07-23 20:31:26 +0700

    refactor: move to separate backend folder

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     machine_id: int = 0
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file="../.env", extra="ignore")
 
 
 settings = Settings()
